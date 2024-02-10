6a622080bc996510bb8a49ce9c7c8419a50c86d5
hazem <hazemdamdoum@gmail.com>
2024-02-10 01:04:30 +0100

    add extracted datetime property

--- airflow/retrieve_data.py
+++ airflow/retrieve_data.py
@@ -1,8 +1,11 @@
 import requests
+import datetime
+import json 
 
 def get_bikepoints_from_api():
     url = 'https://api.tfl.gov.uk/BikePoint/'
     return requests.get(url).json()
+
 
 
 def select_properties(bikepoints_data):
@@ -13,18 +16,24 @@ def select_properties(bikepoints_data):
         item = {
             "id": bikepointInfo["id"],
             "commonName": bikepointInfo["commonName"],
-            "lat": bikepointInfo["lat"],
-            "lon": bikepointInfo["lon"]
+            "lat": str(bikepointInfo["lat"]),
+            "lon": str(bikepointInfo["lon"])
         }
-        
+
+        recent_datetime = None
         for detail in bikepointInfo['additionalProperties']:
             if detail['key'] in ("NbDocks", "NbBikes", "NbEBikes", 
                                 "NbEmptyDocks", "Installed", "Locked"):
                 
                 item[detail["key"]] = detail['value']
+            
+            # Get the most recent modified datetime of the objects
+            modified_datetime = str(detail['modified'])
+            if recent_datetime == None or \
+            recent_datetime < modified_datetime:
+                recent_datetime = modified_datetime    
+            item['extracted_datetime'] = recent_datetime
 
         bikepoints.append(item)
-    
+
     return bikepoints
-
-
