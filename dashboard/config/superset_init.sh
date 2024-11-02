afd000c65761c0d966d25df3afa91ad0cdaa89eb
hazem <hazemdamdoum@gmail.com>
2024-11-02 15:36:31 +0100

    export and import dashboard script. for future : fix order by  in kafka dataset  and review charts.

--- dashboard/config/superset_init.sh
+++ dashboard/config/superset_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # create Admin user, you can read these values from env or anywhere else possible
-superset fab create-admin --username "$ADMIN_USERNAME" --firstname Superset --lastname Admin --email "$ADMIN_EMAIL" --password "$ADMIN_PASSWORD"
+superset fab create-admin --username "$SUPERSET_ADMIN_USERNAME" --firstname Superset --lastname Admin --email "$SUPERSET_ADMIN_EMAIL" --password "$SUPERSET_ADMIN_PASSWORD"
 
 # Upgrading Superset metastore
 superset db upgrade
@@ -9,5 +9,11 @@ superset db upgrade
 # setup roles and permissions
 superset superset init 
 
+# Import the dashboard
+echo "Importing the dashboard..." &
+cd /app/superset & 
+superset import-dashboards --username $SUPERSET_ADMIN_USERNAME -p /app/superset-dashboard-data.zip     
+
 # Starting server
 /bin/sh -c /usr/bin/run-server.sh
+
